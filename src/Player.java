ed679f18ae0f5d61e56686d061362581e31b148f
Jonah Fisher <fisherj2@carleton.edu>
2020-01-22 16:04:47 -0600

    Player interface

---
+++ src/Player.java
@@ -0,0 +1,10 @@
+public interface Player {
+    void income();
+    void foreignAid();
+    void coup();
+    void tax();
+    void assassinate();
+    void exchange();
+    void steal();
+    void challenge();
+}
